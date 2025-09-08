56456f8281c5aecc74c9ecf908aa365fc86998df
gopeshkumarjangid <gopeshsharma594@gmail.com>
2025-09-08 16:40:19 +0530

    First commit

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -2,11 +2,11 @@ const mongoose  = require("mongoose");
 const config = require("config");
 const dbgr = require("debug")("development:mongoose");
 mongoose
-.connect(`${config.get("MONGODB_URI")}/cake`)
-.then(function(){
-dbgr("connected");
-})
-.catch(function(err){
-    console.log(err);
-});
+.connect(`${process.env.MONGODB_URI || config.get("MONGODB_URI")}/cake`);
+// .then(function(){
+// dbgr("connected");
+// })
+// .catch(function(err){
+//     console.log(err);
+// });
 module.exports = mongoose.connection;
